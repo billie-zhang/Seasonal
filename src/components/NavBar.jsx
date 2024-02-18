7e3b2032a68e8f0edd4c19da11f5ccfbc9d44272
Billie Zhang <billie.zhang1504@gmail.com>
2024-02-18 15:44:18 -0500

    update tailwind and clean up

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-42 flex fixed ">
-      <Link to="/">
-        <img src={logo} alt="logo" className="h-8 ml-5 mt-5 pb-5 " />
-      </Link>
+    <div className="w-full h-18 flex fixed ">
+      <div>
+        <Link to="/">
+          <img src={logo} alt="logo" className="h-16 ml-5 mt-5 pb-5 " />
+        </Link>
+      </div>
     </div>
   );
 };
