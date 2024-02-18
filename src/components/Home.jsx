e1697ebff091488784cbd7df4655bbc77addc15b
Billie Zhang <billie.zhang1504@gmail.com>
2024-02-18 15:44:18 -0500

    update tailwind and clean up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,31 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  // Inline styles for elements
-  const containerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "80vh", // Full viewport height
-    backgroundColor: "#f5f5dc", // Beige background
-    color: "#333", // Text color
-    padding: "20px",
-    textAlign: "center",
-    marginBottom: "10px",
-  };
-
-  const welcomeStyle = {
-    fontSize: "24px",
-    fontWeight: "bold",
-    marginBottom: "20px",
-  };
-
-  const instructionStyle = {
-    fontSize: "18px",
-    marginBottom: "30px",
-  };
-
   const buttonStyle = {
     fontSize: "20px",
     padding: "10px 20px",
@@ -38,14 +13,18 @@ const Home = () => {
   };
 
   return (
-    <div style={containerStyle}>
-      <div style={welcomeStyle}>Welcome to Seasonal!</div>
-      <div style={instructionStyle}>
-        Pick what's in season, 4 seasons a year
+    <div>
+      <div className="h-screen flex flex-col items-center justify-center bg-pale-green mt-18">
+        <div className="font-signature m-5 text-3xl font-bold">
+          Welcome to Seasonal!
+        </div>
+        <div className="m-3 text-xl ">
+          Pick what's in season, 4 seasons a year
+        </div>
+        <Link to="/scan">
+          <button style={buttonStyle}>Scan Produce</button>
+        </Link>
       </div>
-      <Link to="/scan">
-        <button style={buttonStyle}>Scan Produce</button>
-      </Link>
     </div>
   );
 };
